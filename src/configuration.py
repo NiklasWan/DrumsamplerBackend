292f9d0a35c7139e115bee66917a9960c9ee6940
NiklasWan <n1k.w@protonmail.com>
2021-07-13 15:25:18 +0200

    fix renaming of repository

--- src/configuration.py
+++ src/configuration.py
@@ -1,8 +1,8 @@
 import os
 
-TAGGING_DIR = '/workspaces/backend/tagging'
-RECOMMENDATION_DIR = '/workspaces/backend/recommendation'
-UPLOAD_FOLDER = '/workspaces/backend/uploads'
+TAGGING_DIR = '/workspaces/DrumsamplerBackend/tagging'
+RECOMMENDATION_DIR = '/workspaces/DrumsamplerBackend/recommendation'
+UPLOAD_FOLDER = '/workspaces/DrumsamplerBackend/uploads'
 ALLOWED_EXTENSIONS = {'wav'}
 DATABASE_FILE = 'sqlite:///drumsamp.db'
 SECRET_TOKEN = os.getenv('TOKEN_SECRET', 'default_secret')
